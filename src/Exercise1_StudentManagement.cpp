1737303c9ed9676658d4caaa9dac3d9720b4c2a1
Israel Gómez <22484886@live.uem.es>
2025-10-07 17:53:07 +0200

    Added User

--- src/Exercise1_StudentManagement.cpp
+++ src/Exercise1_StudentManagement.cpp
@@ -4,7 +4,53 @@
 #include <iostream>
 #include "../include/Exercise1_StudentManagement.h"
 
+class User {
+private:
+    int id;
+    std::string name, surname, role;
+public:
+    User() {
+    };
+
+    User(int id, std::string name, std::string surname, std::string role) {
+        this->id = id;
+        this->name = name;
+        this->surname = surname;
+        this->role = role;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    std::string getName() {
+        return name;
+    }
+
+    std::string getSurname() {
+        return surname;
+    }
+
+    std::string getRole() {
+        return role;
+    }
+
+    void setId(int id) {
+        this->id = id;
+    }
+
+    void setName(std::string &name) {
+        this->name = name;
+    }
+
+    void setSurname(std::string &surname) {
+        this->surname = surname;
+    }
+
+    void setRole(std::string &role) {
+        this->role = role;
+    }
+};
+
 void Exercise1_StudentManagement::run() {
-    std::cout << "Ejecutando Exercise 1 - Student Management..." << std::endl;
-    // Aquí pondrás todo el menú y lógica del ejercicio 1
 }
